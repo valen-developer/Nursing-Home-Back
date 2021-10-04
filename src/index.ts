3143a7ef6cf2398426fa21b7d0f9c266d264c86f
Valentin Valverde <valenreta.developer@gmail.com>
2021-10-04 19:50:25 +0200

    creating server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+import cors from 'cors';
+import { json, urlencoded } from 'express';
+import { enviroment } from './app/config/enviroment';
 import { getContainer } from './app/dic/getContainer';
+import { Server } from './app/server';
+
+const server = new Server(enviroment.port);
+// set middlewares
+server.app.use(urlencoded({ extended: false }));
+server.app.use(json());
+server.app.use(cors());
 
 export const container = getContainer();
+
+server.start();
