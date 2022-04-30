efe78ae0f445c9c35fbcfcdff3ca01a7f8a6d683
Valentin Valverde <valenreta.developer@gmail.com>
2022-05-01 00:35:01 +0200

    set redis url

--- src/helpers/connectRedis.ts
+++ src/helpers/connectRedis.ts
@@ -4,9 +4,11 @@ import { enviroment } from "../app/config/enviroment";
 export const redisClient: redis.RedisClient = redis.createClient({
   host: enviroment.redis.host,
   auth_pass: enviroment.redis.password,
+  url: enviroment.redis.host,
 });
 
 redisClient.on("error", (err) => {
+  console.log(err);
   console.log("Error on redis connection");
 });
 
